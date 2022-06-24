4f782f536f3633a5196b61e181cd5a600cc1bb90
abhi0661236 <ap0661236@gmail.com>
2022-06-24 15:54:24 +0530

    initial commit

---
+++ static/js/script.js
@@ -0,0 +1,8 @@
+window.addEventListener('load',()=>{
+    let toTopBtn = document.getElementById('toTop');
+    toTopBtn.addEventListener('click',()=>{
+        window.location.assign('#');
+    })
+
+
+});
